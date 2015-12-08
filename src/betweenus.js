0cc0f569d6c8614a45e6a7c3704d151ce86eb005
Liran BG <liranbg@gmail.com>
2015-12-08 13:18:57 +0200

    Moved simulation part from betweenus.js to main.js #12

--- src/betweenus.js
+++ src/betweenus.js
@@ -194,11 +194,3 @@ var BetweenUsModule = (function() {
   };
 
 }(BetweenUsModule || {}));
-
-var text_to_encrypt = "BetweenUs";
-var symmetric_key_dictionary = BetweenUsModule.GenerateSymmetricKeyInitDictionary();
-var encrypted_buffer = BetweenUsModule.Symmetric_Encrypt(text_to_encrypt, symmetric_key_dictionary);
-var symmetric_key_shares = BetweenUsModule.SerializedDictionaryToShares(symmetric_key_dictionary, 5, 2, 10);
-var from_shares_symmetric_key_dictionary = BetweenUsModule.SharesToSerializedDictionary(symmetric_key_shares.slice(0, 3));
-var decrypted_buffer = BetweenUsModule.Symmetric_Decrypt(encrypted_buffer, from_shares_symmetric_key_dictionary);
-console.log(decrypted_buffer.toString('utf8'));
