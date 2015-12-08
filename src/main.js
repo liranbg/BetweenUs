ce56e2873015bfea4c83bf657bf1aee39c16754c
Nadav Luzzato <nadav2051@gmail.com>
2015-12-08 17:31:04 +0200

    Issue #12 - Added verbosity throughout the encryption/decryption flow - not done.

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ function decrypt_with_private_key(enc_share, private_key) {
 }
 
 
-var text_to_encrypt = "BetweenUs";
+var text_to_encrypt = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 var client_1 = {
   id: 'client1',
@@ -55,16 +55,23 @@ Scenario: [Encryption\Creating transaction] -  Client 1 wants to encrypt the tex
  */
 
 //Client 1 creates symmetric key [represented as dictoinary. holds key and initial vector]
+console.log('[+] Starting BetweenUs flow on text: \n"' + text_to_encrypt + '"');
+console.log('[+] Generating Symmetric Key...');
 var symmetric_key = BetweenUsModule.GenerateSymmetricKeyDictionary();
+console.log('[+] Done.\n[+] Key: ' + symmetric_key);
 
 //Client 1 encryptes [text_to_encrypt] with the symmetric key from above
+console.log('[+] Generating cipher text using previously generated symmetric key...');
 var encrypted_buffer = BetweenUsModule.SymmetricEncrypt(text_to_encrypt, symmetric_key);
-
+console.log('[+] Encryption done.\n[+] Cipher text:\n' + encrypted_buffer.toString('hex') );
 //Client 1 gets client list to share the secret with. then encryptes each share with client's public key accordingly
 var clients_to_share_with = [client_1, client_2, client_3];
+console.log('[+] Using Shamir\'s Secret Sharing to split symmetric key into shares.');
 var shares = BetweenUsModule.SerializedDictionaryToShares(symmetric_key, clients_to_share_with.length, 2, 0);
+console.log('[+] Done.\n[+] Shares:');
 var assigned_shares = [];
 for (var i in shares) {
+  console.log('ID: ' + clients_to_share_with[i].id + ', Share: ' + shares[i]);
   assigned_shares.push({
     belong_to: clients_to_share_with[i].id,
     share: encrypt_with_public_key(shares[i], clients_to_share_with[i].assymetric_key.public_key)
@@ -92,4 +99,5 @@ shares_to_derypt = [clients_to_share_with[0].owned_share, clients_to_share_with[
 
 var from_shares_symmetric_key_dictionary = BetweenUsModule.SharesToSerializedDictionary(shares_to_derypt);
 var decrypted_buffer = BetweenUsModule.SymmetricDecrypt(encrypted_buffer, from_shares_symmetric_key_dictionary);
+
 console.log(decrypted_buffer.toString('utf8'));
