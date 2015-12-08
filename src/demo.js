67077f45b67fe4f8ac68dbd3698a04c62cecfc8b
Nadav <Nadav2051@gmail.com>
2015-12-08 13:03:45 +0200

    Issue #11 - Documenting and refactoring for better type safety in functions.

--- src/demo.js
+++ src/demo.js
@@ -36,6 +36,34 @@ var BetweenUsModule = (function() {
 
   _secrets.init(defaults.max_bit);
 
+/**
+ * Check type and expected type. throws an error if types isn't matching.
+ * @param  {variable} type          [Variable to check for type.]
+ * @param  {string}   expected_type [String describing the expected type.]
+ * @return {None}                   [Return true if types match, throws an error otherwise.]
+ */
+  var _type_assert = function(type, expected_type) {
+      /* Check special case: buffer */
+      if (Buffer.isBuffer(expected_type)) {
+        if (!Buffer.isBuffer(type)) {
+          throw new Error('Type Error. Expected: Buffer, Received: ' + typeof type +'.');
+        }
+        else {
+          return true;
+        }
+      }
+      else if (typeof type != typeof expected_type) {
+          throw new Error('Type Error. Expected: ' + typeof expected_type +', Received: ' + typeof type +'.');
+      }
+  };
+
+  var _types = {
+        dictionary: {},
+        number: 1,
+        string: "s",
+        buffer: Buffer("s"),
+        obj: {}
+      };
   /**
    * Serializes a dictionary into a stringfied JSON.
    * @param  {dictionary} dictionary  [contains a .key{Buffer} (random passphrase) and .iv{Buffer} (init vector) keys.]
@@ -43,6 +71,7 @@ var BetweenUsModule = (function() {
    */
   var _serialized_symmetric_dictionary = function(dictionary) {
     var serialized = {};
+    _type_assert(dictionary, _types.dictionary);
     serialized.key = dictionary.key.toString('hex');
     serialized.iv = dictionary.iv.toString('hex');
     return JSON.stringify(dictionary);
@@ -55,12 +84,14 @@ var BetweenUsModule = (function() {
    */
   var _deserialized_symmetric_dictionary = function(json_string) {
     var deserialized = {};
+    _type_assert(json_string, _types.string);
     try {
       parsed_dictionary = JSON.parse(json_string);
     } catch (e) {
       throw "Invalid JSON Object to parse.";
     }
     /* Convert the hex strings into Buffer types (required by the AES algo.)  */
+    _type_assert(parsed_dictionary, _types.dictionary);
     deserialized.key = Buffer(parsed_dictionary.key);
     deserialized.iv = Buffer(parsed_dictionary.iv);
     return deserialized;
@@ -75,6 +106,8 @@ var BetweenUsModule = (function() {
   var GenerateSymmetricKeyInitDictionary = function() {
     var symmetric_key = _crypto.randomBytes(defaults.symmetric_algorithm.passphrase_byte_size);
     var initialization_vector = _crypto.randomBytes(defaults.symmetric_algorithm.init_vector_byte_size);
+    _type_assert(symmetric_key,         _types.buffer);
+    _type_assert(initialization_vector, _types.buffer);
     return _serialized_symmetric_dictionary({
       key: symmetric_key,
       iv: initialization_vector
@@ -91,9 +124,11 @@ var BetweenUsModule = (function() {
  * @return {Buffer}                              [Buffer containing the output data - the cipher text.]
  */
   var Symmetric_Encrypt = function(message, symmetric_key_dictionary) {
+    _type_assert(message, _types.string);
     var key_object = _deserialized_symmetric_dictionary(symmetric_key_dictionary);
     var cipher = _crypto.createCipheriv(defaults.symmetric_algorithm.algorithm_name, key_object.key, key_object.iv);
     var crypted = Buffer.concat([cipher.update(message), cipher.final()]);
+    _type_assert(crypted, _types.buffer);
     return crypted;
   };
 
@@ -105,9 +140,12 @@ var BetweenUsModule = (function() {
  * @return {Buffer}                             [Buffer containing the decrypted cipher text.]
  */
   var Symmetric_Decrypt = function(encrypted_message, symmetric_key_dictionary) {
+      _type_assert(encrypted_message,         _types.buffer);
+      _type_assert(symmetric_key_dictionary,  _types.string);
       var key_object = _deserialized_symmetric_dictionary(symmetric_key_dictionary);
       var decipher = _crypto.createDecipheriv(defaults.symmetric_algorithm.algorithm_name, key_object.key, key_object.iv);
       var dec = Buffer.concat([decipher.update(encrypted_message), decipher.final()]);
+      _type_assert(dec, _types.buffer);
       return dec;
     };
 
@@ -125,9 +163,10 @@ var BetweenUsModule = (function() {
  * @return {Object}                       [Object that contains the list of the actual shares in hex string forms.]
  */
   var SerializedDictionaryToShares = function(serialized_dictionary, shares, threshold, zeropadding) {
-    if (typeof serialized_dictionary !== "string") {
-      throw "Invalid Input: String Input Only";
-    }
+     _type_assert(serialized_dictionary,  _types.string);
+     _type_assert(shares,                 _types.number);
+     _type_assert(threshold,              _types.number);
+     _type_assert(zeropadding,            _types.number);
      var share_list = _secrets.share(_secrets.str2hex(serialized_dictionary), shares, threshold, zeropadding);
      return share_list;
   };
@@ -138,33 +177,17 @@ var BetweenUsModule = (function() {
    * @return {string}        [Resolve the shares into the serialized dictionary that contains the encryption data.]
    */
   var SharesToSerializedDictionary = function(shares) {
-    if (typeof shares !== "object") {
-      throw "Invalid Input: Shares Only";
-    }
+    _type_assert(shares, _types.obj);
     var hex_string = _secrets.combine(shares);
     return _secrets.hex2str(hex_string);
   };
 
   /**
-   * [function description]
-   * @param  {[type]} id     [description]
-   * @param  {[type]} shares [description]
-   * @return {[type]}        [description]
+   * BetweenUs Module API
    */
-  var GetNewShare = function(id, shares) {
-      if (typeof shares !== "object") {
-        throw "Invalid Input: Shares Only";
-      }
-      return _secrets.newShare(id, shares); // => newShare = '808xxx...xxx'
-    };
-
-    /**
-     * BetweenUs Module API
-     */
   return {
     SerializedDictionaryToShares: SerializedDictionaryToShares,
     SharesToSerializedDictionary: SharesToSerializedDictionary,
-    GetNewShare: GetNewShare,
     GenerateSymmetricKeyInitDictionary: GenerateSymmetricKeyInitDictionary,
     Symmetric_Encrypt: Symmetric_Encrypt,
     Symmetric_Decrypt: Symmetric_Decrypt
