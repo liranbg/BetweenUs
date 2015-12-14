27186e8c78a0f5c36053d1fdc1b93392b955b830
Liran BG <liranbg@gmail.com>
2015-12-14 15:37:51 +0200

    Added moment and winston packages for loggings. Refactored printing functions to use logging package #12

--- src/main.js
+++ src/main.js
@@ -1,5 +1,39 @@
 var BetweenUsModule = require('./betweenus');
 var RSA = require('node-rsa');
+var winston = require('winston');
+var moment = require('moment');
+var logger = new(winston.Logger)({
+  transports: [
+    new(winston.transports.File)({
+      // humanReadableUnhandledException: true,
+      // handleExceptions: true,
+      colorize: true,
+      json: false,
+      level: 'debug',
+      name: 'info-file',
+      filename: 'info.json',
+      timestamp: function() {
+        return moment(new Date()).format("YYYY-MM-dTHH:mm:ss:SSS");
+      },
+      formatter: function(options) {
+        // Return string will be passed to logger.
+        return options.timestamp() + ' ' + options.level.toUpperCase() + ' ' + (undefined !== options.message ? options.message : '') +
+          (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '');
+      }
+    }),
+    new(winston.transports.Console)({
+      level: 'error',
+      timestamp: function() {
+        return moment(new Date()).format("YYYY-MM-dTHH:mm:ss:SSS");
+      },
+      formatter: function(options) {
+        // Return string will be passed to logger.
+        return options.timestamp() + ' ' + options.level.toUpperCase() + ' ' + (undefined !== options.message ? options.message : '') +
+          (options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '');
+      }
+    })
+  ]
+});
 var rsa_bits = 512;
 
 function encrypt_with_public_key(share, rsa_key) {
@@ -9,7 +43,6 @@ function encrypt_with_public_key(share, rsa_key) {
 function decrypt_with_private_key(enc_share, rsa_key) {
   return rsa_key.decrypt(enc_share, 'utf8');
 }
-
 var text_to_encrypt = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 var client_1 = {
@@ -23,7 +56,9 @@ var client_1 = {
 var client_2 = {
   id: 'client2',
   assymetric_key: {
-    rsa_key: new RSA({b: rsa_bits})
+    rsa_key: new RSA({
+      b: rsa_bits
+    })
   },
   owned_share: null,
   share_hold: []
@@ -31,7 +66,9 @@ var client_2 = {
 var client_3 = {
   id: 'client3',
   assymetric_key: {
-    rsa_key: new RSA({b: rsa_bits})
+    rsa_key: new RSA({
+      b: rsa_bits
+    })
   },
   owned_share: null,
   share_hold: []
@@ -47,23 +84,27 @@ Scenario: [Encryption\Creating transaction] -  Client 1 wants to encrypt the tex
  */
 
 //Client 1 creates symmetric key [represented as dictoinary. holds key and initial vector]
-console.log('[+] Starting BetweenUs flow on text: \n"' + text_to_encrypt + '"');
-console.log('[+] Generating Symmetric Key...');
+logger.info('Starting BetweenUs flow on text:');
+logger.info('"' + text_to_encrypt + '"');
+logger.info('Generating Symmetric Key...');
 var symmetric_key = BetweenUsModule.GenerateSymmetricKeyDictionary();
-console.log('[+] Done.\n[+] Key: ' + symmetric_key);
+logger.info('Done.');
+logger.info('Key: ' + symmetric_key);
 
 //Client 1 encryptes [text_to_encrypt] with the symmetric key from above
-console.log('[+] Generating cipher text using previously generated symmetric key...');
+logger.info('Generating cipher text using previously generated symmetric key...');
 var encrypted_buffer = BetweenUsModule.SymmetricEncrypt(text_to_encrypt, symmetric_key);
-console.log('[+] Encryption done.\n[+] Cipher text:\n' + encrypted_buffer.toString('hex') );
+logger.info('Encryption done.');
+logger.info('Cipher text: ' + encrypted_buffer.toString('hex'));
 //Client 1 gets client list to share the secret with. then encryptes each share with client's public key accordingly
 var clients_to_share_with = [client_1, client_2, client_3];
-console.log('[+] Using Shamir\'s Secret Sharing to split symmetric key into shares.');
+logger.info('Using Shamir\'s Secret Sharing to split symmetric key into shares.');
 var shares = BetweenUsModule.SerializedDictionaryToShares(symmetric_key, clients_to_share_with.length, 2, 0);
-console.log('[+] Done.\n[+] Shares:');
+logger.info('Done.');
+logger.info('Shares:');
 var assigned_shares = [];
 for (var i in shares) {
-  console.log('ID: ' + clients_to_share_with[i].id + ', Share: ' + shares[i]);
+  logger.info('ID: ' + clients_to_share_with[i].id + ', Share: ' + shares[i]);
   assigned_shares.push({
     belong_to: clients_to_share_with[i].id,
     share: encrypt_with_public_key(shares[i], clients_to_share_with[i].assymetric_key.rsa_key)
@@ -82,7 +123,7 @@ for (var i in clients_to_share_with) {
 //Checking all clients has correct shares. if not prints client id
 for (var i in clients_to_share_with) {
   if (shares.indexOf(clients_to_share_with[i].owned_share) == -1) {
-    console.log(clients_to_share_with[i].id);
+    logger.info(clients_to_share_with[i].id);
   }
 }
 
@@ -92,4 +133,4 @@ shares_to_derypt = [clients_to_share_with[0].owned_share, clients_to_share_with[
 var from_shares_symmetric_key_dictionary = BetweenUsModule.SharesToSerializedDictionary(shares_to_derypt);
 var decrypted_buffer = BetweenUsModule.SymmetricDecrypt(encrypted_buffer, from_shares_symmetric_key_dictionary);
 
-console.log(decrypted_buffer.toString('utf-8'));
+logger.info(decrypted_buffer.toString('utf-8'));
