4c30f31aa10c7a68d93aceb54836edae18d91f09
Nadav <Nadav2051@gmail.com>
2016-01-11 18:22:25 +0200

    New Client for testing - supports registering user

---
+++ src/client_side/index.js
@@ -0,0 +1,18 @@
+var server = "http://localhost:3000"
+function RegisterFormOnClick () {
+  var email = document.getElementById("form_register_email").value;
+  var password =  document.getElementById("form_register_password").value;
+  var public_key = document.getElementById("form_register_public_key").value;
+  var json_data = {email: email, password: password, public_key: public_key };
+  $.ajax({
+    type: "POST",
+    url: server + "/users/register_user",
+    data: json_data,
+    success: function(data, status, xhr) {
+      document.getElementById("textfield_register_user").value = data.message;
+    },
+    error: function(xhr, status, error) {
+        document.getElementById("textfield_register_user").value = JSON.parse(xhr.responseText).error;
+    }
+});
+}
