449a19c81e6f98807ccdd50410a05269929cef6c
Nadav <Nadav2051@gmail.com>
2016-01-24 12:07:39 +0200

    #12 - For presentation sake, merged two functions into one for readability.

--- src/client_side/index.js
+++ src/client_side/index.js
@@ -320,11 +320,14 @@ function RequestShareStashEnableButton(threshold_reached_span_id, transaction_id
 function RequestAndResolveShareStash(transaction_id, secret_output_textarea_id) {
     $.ajax({
         /* Issue a get request to the server, which return a structure of this form:
-         {"success":true,"shares_list":[
-            {"bits":8,"id":1,"data":"share1_that's_with_requesting_user_public_key"},
-            {"bits":8,"id":2,"data":"share2_that's_with_requesting_user_public_key"},
-            {"bits":8,"id":3,"data":"share3_that's_with_requesting_user_public_key"}
-            ]
+         {
+             "success":true,
+             "shares_list":
+                [
+                    {"bits":8,"id":1,"data":"share1_that's_with_requesting_user_public_key"},
+                    {"bits":8,"id":2,"data":"share2_that's_with_requesting_user_public_key"},
+                    {"bits":8,"id":3,"data":"share3_that's_with_requesting_user_public_key"}
+                ]
          }
          */
         type: "GET",
@@ -354,7 +357,45 @@ function RequestAndResolveShareStash(transaction_id, secret_output_textarea_id) 
             /* Once we have the symmetric key, we query the server for the actual encrypted data, solving it using
                the symmetric key we constructing, and outputting it to the supplied output textarea.
              */
-            SolveTransactionWithSymmetricKey(transaction_id, symmetric_key, secret_output_textarea_id);
+
+            /* The next fragment of code is usually executed in another function, but for the sake of verbosity, we've
+               included the function body here.
+               Func Call: SolveTransactionWithSymmetricKey(transaction_id, symmetric_key, secret_output_textarea_id);
+             */
+            $.ajax({
+                /* Query the server for the cipher data, which returns a structure of this form:
+                 {
+                     "success":true,
+                     "cipher":
+                        {
+                            "type":"String",
+                            "data":"\u0002F°r\u0007êÒ\u0017Â\u000b\u0015\u0015_¡ÑÐ!»..\u0017tö"
+                        }
+                 }
+                 */
+                type: "GET",
+                url: server + "/transactions/get_cipher_data?transaction_id=" + transaction_id,
+                dataType:'json',
+                xhrFields: {withCredentials: true},
+                success: function(data, status, xhr) {
+                    /* extract relevant data, and use the BetweenUs module SymmetricDecrypt function.
+                       SymmetricDecrypt arguments are:
+                        * param1: uIntArray/Buffer of the cipher data.
+                        * param2: String representing the serialized AES key data
+                        * return: uIntArray/Buffer containing the plain text.
+                     */
+                    var cipher_text = data.cipher.data,
+                        output_text = betweenus.SymmetricDecrypt(Util_Text2uIntArray(cipher_text), symmetric_key);
+                    output_text = Util_uIntArray2Text(output_text);
+                    /* Done decrypting and converting data.
+                       Output the data for the user to see.
+                    */
+                    $("#" + secret_output_textarea_id).prop("hidden", false);
+                    $("#" + secret_output_textarea_id).val(output_text);
+                },
+                error: function(xhr, status, error) {
+                    alert("Error fetching cipher data.");
+                }});
         },
         error: function(xhr, status, error) {
             alert("An error occured while fetching the share stash.");
